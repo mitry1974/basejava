7b913e75b893e9a3d1ab8e2d5f827fdbc3e8b951
mitry1974 <36993758+mitry1974@users.noreply.github.com>
2018-06-06 16:52:37 +0300

    Homework for the lesson 3. Implementation of the SortedArrayStorage class.

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -1,20 +1,22 @@
+import java.util.Arrays;
+
 /**
  * Test ru.javawebinar.basejava.storage.ArrayStorage
  */
 public class MainTestArrayStorage {
-    static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+    static final Storage ARRAY_STORAGE = new SortedArrayStorage();
 
     public static void main(String[] args) {
         Resume r1 = new Resume();
         r1.setUuid("uuid1");
         Resume r2 = new Resume();
         r2.setUuid("uuid2");
-        Resume r3 = new Resume();
-        r3.setUuid("uuid3");
+        Resume r5 = new Resume();
+        r5.setUuid("uuid5");
 
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
-        ARRAY_STORAGE.save(r3);
+        ARRAY_STORAGE.save(r5);
 
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
         System.out.println("Size: " + ARRAY_STORAGE.size());
@@ -24,6 +26,19 @@ public class MainTestArrayStorage {
         printAll();
         ARRAY_STORAGE.delete(r1.getUuid());
         printAll();
+
+        Resume r4 = new Resume();
+        r4.setUuid("uuid4");
+        ARRAY_STORAGE.save(r4);
+        printAll();
+
+        ARRAY_STORAGE.save(r1);
+        printAll();
+
+        ARRAY_STORAGE.save(r1);
+        printAll();
+
+
         ARRAY_STORAGE.clear();
         printAll();
 
--- src/SortedArrayStorage.java
+++ src/SortedArrayStorage.java
@@ -3,27 +3,55 @@ import java.util.Arrays;
 public class SortedArrayStorage extends AbstractArrayStorage{
     @Override
     public void clear() {
-
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     @Override
-    public void update(Resume r) {
-
+    public void update(Resume resume) {
+        int index = getIndex(resume.getUuid());
+        if(index < 0){
+            System.out.println("Resume:delete, uuid - " + resume.getUuid() + " not found!");
+            return;
+        }
+        storage[index] = resume;
     }
 
     @Override
-    public void save(Resume r) {
+    public void save(Resume resume) {
+        if (size >= STORAGE_LIMIT - 1){
+            System.out.println("Resume:save, can't insert resume. Maximum amount is reached!");
+            return;
+        }
 
+        int indexToInsert = Arrays.binarySearch(storage, 0, size, resume);
+        if(indexToInsert >= 0){
+            System.out.println("Resume:save, uuid - always in the database!");
+            return;
+        }
+
+        indexToInsert = Math.abs(indexToInsert + 1);
+        System.arraycopy(storage, indexToInsert, storage, indexToInsert + 1, size - indexToInsert);
+        storage[indexToInsert] = resume;
+        size++;
     }
 
     @Override
     public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if(index < 0){
+            System.out.println("Resume:delete, uuid - " + uuid + " not found!");
+            return;
+        }
 
+        System.arraycopy(storage, index + 1, storage, index, size - index - 1);
+        storage[size - 1] = null;
+        size --;
     }
 
     @Override
     public Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     @Override
