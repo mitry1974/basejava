50385d1dd65886242150472675be73f2638d3ef3
mitry1974 <36993758+mitry1974@users.noreply.github.com>
2018-06-07 00:43:02 +0300

    HW4, call toString method via reflection

---
+++ src/MainReflection.java
@@ -0,0 +1,22 @@
+import model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Resume r = new Resume();
+        Field field = r.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println(field.get(r));
+        field.set(r, "new_uuid");
+        // TODO : invoke r.toString via reflection
+        Class aclass = r.getClass();
+        Method method = aclass.getMethod("toString");
+        Object[] argsToInvoke = new Object[]{};
+        System.out.println((String) method.invoke(r, argsToInvoke));
+    }
+}
