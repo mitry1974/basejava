f87184561a4c3f1a94daa78aa938aa506731e959
mitry1974 <36993758+mitry1974@users.noreply.github.com>
2018-06-06 23:26:37 +0300

    HW4 branch, commits according to the lesson 4

--- src/storage/ArrayStorage.java
+++ src/storage/ArrayStorage.java
@@ -6,48 +6,23 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
+
+/**
+ * Array based storage for Resumes
+ */
 public class ArrayStorage extends AbstractArrayStorage {
 
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
+    @Override
+    protected void fillDeletedElement(int index) {
+        storage[index] = storage[size - 1];
     }
 
-    public void update(Resume resume) {
-        int index = getIndex(resume.getUuid());
-        if (index == -1) {
-            System.out.println("Resume " + resume.getUuid() + " not exist");
-        } else {
-            storage[index] = resume;
-        }
+    @Override
+    protected void insertElement(Resume r, int index) {
+        storage[size] = r;
     }
 
-    public void save(Resume resume) {
-        if (getIndex(resume.getUuid()) != -1) {
-            System.out.println("Resume " + resume.getUuid() + " already exist");
-        } else if (size >= STORAGE_LIMIT) {
-            System.out.println("Storage overflow");
-        } else {
-            storage[size] = resume;
-            size++;
-        }
-    }
-
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index == -1) {
-            System.out.println("Resume " + uuid + " not exist");
-        } else {
-            storage[index] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
-        }
-    }
-
-    public Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size);
-    }
-
+    @Override
     protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].getUuid())) {
--- src/storage/SortedArrayStorage.java
+++ src/storage/SortedArrayStorage.java
@@ -4,63 +4,26 @@ import model.Resume;
 import java.util.Arrays;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
+
     @Override
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
+    protected void fillDeletedElement(int index) {
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(storage, index + 1, storage, index, numMoved);
+        }
     }
 
     @Override
-    public void update(Resume resume) {
-        int index = getIndex(resume.getUuid());
-        if (index < 0) {
-            System.out.println("Resume:delete, uuid - " + resume.getUuid() + " not found!");
-            return;
-        }
-        storage[index] = resume;
-    }
-
-    @Override
-    public void save(Resume resume) {
-        if (size >= STORAGE_LIMIT - 1) {
-            System.out.println("Resume:save, can't insert resume. Maximum amount is reached!");
-            return;
-        }
-
-        int indexToInsert = Arrays.binarySearch(storage, 0, size, resume);
-        if (indexToInsert >= 0) {
-            System.out.println("Resume:save, uuid - always in the database!");
-            return;
-        }
-
-        indexToInsert = Math.abs(indexToInsert + 1);
-        System.arraycopy(storage, indexToInsert, storage, indexToInsert + 1, size - indexToInsert);
-        storage[indexToInsert] = resume;
-        size++;
-    }
-
-    @Override
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index < 0) {
-            System.out.println("Resume:delete, uuid - " + uuid + " not found!");
-            return;
-        }
-
-        System.arraycopy(storage, index + 1, storage, index, size - index - 1);
-        storage[size - 1] = null;
-        size--;
-    }
-
-    @Override
-    public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
+    protected void insertElement(Resume r, int index) {
+//      http://codereview.stackexchange.com/questions/36221/binary-search-for-inserting-in-array#answer-36239
+        int insertIdx = -index - 1;
+        System.arraycopy(storage, insertIdx, storage, insertIdx + 1, size - insertIdx);
+        storage[insertIdx] = r;
     }
 
     @Override
     protected int getIndex(String uuid) {
-        Resume searchKey = new Resume();
-        searchKey.setUuid(uuid);
+        Resume searchKey = new Resume(uuid);
         return Arrays.binarySearch(storage, 0, size, searchKey);
     }
 }
