f1d77e6403f87aa9c92716a4292083a70055a721
mitry1974 <36993758+mitry1974@users.noreply.github.com>
2018-06-14 08:57:39 +0300

    AbstractArrayStorageTest::saveOverflow add Assert.fail call in case of unexpected exception

--- test/storage/AbstractArrayStorageTest.java
+++ test/storage/AbstractArrayStorageTest.java
@@ -4,6 +4,8 @@ import exception.StorageException;
 import model.Resume;
 import org.junit.Test;
 
+import static org.junit.Assert.fail;
+
 public abstract class AbstractArrayStorageTest extends AbstractStorageTest {
     AbstractArrayStorageTest(Storage storage) {
         super(storage);
@@ -11,8 +13,12 @@ public abstract class AbstractArrayStorageTest extends AbstractStorageTest {
 
     @Test(expected = StorageException.class)
     public void saveOverflow() {
-        for (int i = storage.size(); i < AbstractArrayStorage.STORAGE_LIMIT; i++) {
-            storage.save(new Resume("uuid" + (i + 1), "Unknow man " + (i + 1)));
+        try{
+            for (int i = storage.size(); i < AbstractArrayStorage.STORAGE_LIMIT; i++) {
+                storage.save(new Resume("uuid" + (i + 1), "Unknow man " + (i + 1)));
+            }
+        }catch (Exception e){
+            fail();
         }
 
         storage.save(new Resume("uuid10001", "Unknow man 10001"));
