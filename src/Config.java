632e39b71721c6ac778f2225a11a3ef60502c468
mitry1974 <36993758+mitry1974@users.noreply.github.com>
2018-06-30 14:38:32 +0300

    database commits

--- src/Config.java
+++ src/Config.java
@@ -1,14 +1,17 @@
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.*;
 
 public class Config {
     private static final Config INSTANCE = new Config();
+    private static final File PROPS = new File(".\\storage\\resumes.properties");
 
     public static Config get() {
         return INSTANCE;
     }
 
     private Config() {
-        try(InputStream is = new FileInputStream("./config/resumes.properties")){}
+        try(InputStream is = new FileInputStream(PROPS)){
+        } catch (IOException e) {
+            throw new IllegalStateException("invalid config file name" + PROPS.getAbsolutePath());
+        }
     }
 }
